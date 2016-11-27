96963a70c7167757b1cdd4c2a94933b0f11b4a7c
luoyu <z_y_ang@163.com>
2016-11-27 19:41:28 +0800

    define json view resolver withour debug

--- src/main/java/site/luoyu/controller/MainController.java
+++ src/main/java/site/luoyu/controller/MainController.java
@@ -3,6 +3,7 @@ package site.luoyu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 /**
  * Created by xd on 2016/9/16.
  */
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
     @RequestMapping()
-    public String homeRedirect(){
+    public String homeRedirect() {
         return "redirect:/userAction/MainPage";
     }
 }
---
+++ src/main/java/site/luoyu/model/LoginResult.java
@@ -0,0 +1,18 @@
+package site.luoyu.model;
+
+/**
+ * Created by xd on 2016/11/27.
+ * 登陆结果
+ */
+public class LoginResult {
+
+    boolean result;
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+}
