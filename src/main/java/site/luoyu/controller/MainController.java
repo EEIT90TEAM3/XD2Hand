634b71e1f32995f062976fcae3315d983a58a1a4
luoyu <z_y_ang@163.com>
2017-03-08 16:46:33 +0800

    cherry pick to JPA
    (cherry picked from commit 6e23a66)

--- src/main/java/site/luoyu/controller/MainController.java
+++ src/main/java/site/luoyu/controller/MainController.java
@@ -2,12 +2,16 @@ package site.luoyu.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import site.luoyu.exampleForLearn.RedisCache;
+import site.luoyu.exampleForLearn.RedisExample.Receiver;
 import site.luoyu.model.User;
 import javax.validation.Valid;
 
@@ -19,9 +23,17 @@ import javax.validation.Valid;
 @RequestMapping("/")
 public class MainController {
 
+    @Autowired
+    private RedisCache cache;
+
+    @Autowired
+    private Receiver receiver;
 
     @RequestMapping()
     public String homeRedirect() {
+        cache.testCache("1");
+        receiver.redisPut();
+        System.out.println(receiver.redisGet());
         return "redirect:/userAction/MainPage";
     }
 }
